e66fd8c25dd15178229153db37275daba16fd751
charlie.elliott <charlie.elliott@connectorsubsea.com>
2026-02-02 11:55:22 +0000

    Initial test setup

---
+++ install-script.sh
@@ -0,0 +1,16 @@
+# install dependencies
+sudo pacman -S vi base-devel git xorg-server xorg-xinit ttf-liberation libxinerama xorg-init
+
+
+
+
+# build the desktop
+cd arch-desktop-env/dwm
+sudo make clean install
+
+cd ../dmenu
+sudo make clean install
+
+cd ../st
+sudo make clean install
+
